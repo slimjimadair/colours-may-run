79ebeb76d6d1656ac2400a35971a4c2997b834dc
slimjimadair <slimjimadair@gmail.com>
2020-06-07 17:33:51 +0100

    Basic 2D Movement
    Created basic placeholder assets and added 2d movement (from Blackthornprod tutorial).

---
+++ Colours May Run/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public int numberOfJumps;
+    private int extraJumps;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if ((!facingRight && moveInput > 0) || (facingRight && moveInput < 0))
+        {
+            Flip();
+        }
+    }
+
+    private void Update()
+    {
+        if(isGrounded)
+        {
+            extraJumps = numberOfJumps;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            Debug.Log(extraJumps);
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps -= 1;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+}
